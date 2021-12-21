2075c859bf839fd3c3eddaf5eda387d29dfc9330
DESKTOP-1DPJ3NA\Arturo <artuurodrt@gmail.com>
2021-12-21 10:37:21 -0600

    C program of how the comma operator is used

---
+++ Advanced C programming Course/Comma_Operator/main.c
@@ -0,0 +1,30 @@
+/* Comma Operator
+*
+*  Created by Arturo de los Rios on 21/12/21.
+*
+* C code of how the comma operator is used.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    while(i < 100){
+        sum += data[i], ++i;
+    }
+
+    int i(5, 10) //10 is assigned to i
+
+    /*f1 is going to ba called and evaluated first
+    * then f2 and its return value is going to be assigned to j*/
+    int j(f1(), f2());
+
+    x =(y = 3, (z = ++y +2) +5); //x = 11
+
+    for(i = 0, j = 100; i != 10; ++i, j -= 10) //for loop with the comma operator
+
+
+
+    return 0;
+}
