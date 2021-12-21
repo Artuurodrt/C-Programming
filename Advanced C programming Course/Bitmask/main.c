63f9a8c9b51b955b529a5b625d101181e540ad4d
DESKTOP-1DPJ3NA\Arturo <artuurodrt@gmail.com>
2021-12-20 18:57:56 -0600

    C code of how bitmasks are used.

---
+++ Advanced C programming Course/Bitmask/main.c
@@ -0,0 +1,14 @@
+/* Bitmask
+*
+*  Created by Arturo de los Rios on 20/12/21.
+*
+* C code of how bitmasks are used.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
