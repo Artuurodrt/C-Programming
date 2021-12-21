a81666ed8d64b7ff85fd474945eaf75e2bfe7a5d
DESKTOP-1DPJ3NA\Arturo <artuurodrt@gmail.com>
2021-12-20 19:23:42 -0600

    C code of how bitmasks are used.

--- Advanced C programming Course/Bitmask/main.c
+++ Advanced C programming Course/Bitmask/main.c
@@ -9,6 +9,11 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
+    int flags = 15; //    0000 1111
+    int mask = 182; //    1011 0110
+
+    flags = flags | mask; //1011 1111
+
     return 0;
 }
